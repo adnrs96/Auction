ddd5f3e1be8f65242dadc920ab74d8b759133967
Aditya Bansal <adi.bansal241996@gmail.com>
2019-04-18 21:10:40 +0530

    auctioneer: Export auctionRequest, auctionResponse and biddingService.

--- cmd/auctioneer/main.go
+++ cmd/auctioneer/main.go
@@ -23,22 +23,22 @@ var (
 
 type AuctionService interface {
   ValidateAdPlacementId(string) bool
-  GetBiddingServices(string) []biddingService
-  ConductAuction(string) auctionResponse
-  MakeBidRequest(biddingService, string, chan auctionResponse)
+  GetBiddingServices(string) []BiddingService
+  ConductAuction(string) AuctionResponse
+  MakeBidRequest(BiddingService, string, chan AuctionResponse)
 }
 
 type auctionService struct {}
 
-type biddingService struct {
+type BiddingService struct {
   Addr string
 }
 
-type auctionRequest struct {
+type AuctionRequest struct {
 	AdPlacementId string `json:"AdPlacementId"`
 }
 
-type auctionResponse struct {
+type AuctionResponse struct {
 	AdId   string `json:"AdId"`
 	BidPrice float64 `json:"BidPrice"`
 }
@@ -49,22 +49,22 @@ func (auctionService) ValidateAdPlacementId(adPlacementId string) bool {
   return true
 }
 
-func (auctionService) GetBiddingServices(adPlacementId string) []biddingService {
+func (auctionService) GetBiddingServices(adPlacementId string) []BiddingService {
   // Ideally we would do some sorta DB query here to obtain compatible bidding
   // services but for now lets just accept bidding service host from CLI.
   biddingAddress := os.Getenv("BIDDING_SERVICE_URL")
   if biddingAddress == "" {
     biddingAddress = "biddingservice"
   }
-  bidding_services := []biddingService{
+  bidding_services := []BiddingService{
     {biddingAddress},
   }
   return bidding_services
 }
 
-func (auctionService) MakeBidRequest(bs biddingService, adPlacementId string, bids chan auctionResponse) {
+func (auctionService) MakeBidRequest(bs BiddingService, adPlacementId string, bids chan AuctionResponse) {
   url := fmt.Sprintf("http://%s/placebid", bs.Addr)
-  jsonValue, _ := json.Marshal(auctionRequest{adPlacementId})
+  jsonValue, _ := json.Marshal(AuctionRequest{adPlacementId})
   timeout := time.Duration(200 * time.Millisecond)
   client := http.Client{
     Timeout: timeout,
@@ -73,33 +73,33 @@ func (auctionService) MakeBidRequest(bs biddingService, adPlacementId string, bi
   httpResponse, err := client.Post(url, "application/json", bytes.NewBuffer(jsonValue))
 
   if err != nil {
-    bids <- auctionResponse{}
+    bids <- AuctionResponse{}
     return
   }
   defer httpResponse.Body.Close()
   if httpResponse.StatusCode == http.StatusOK {
-    var auction_response auctionResponse
+    var auction_response AuctionResponse
     body, _ := ioutil.ReadAll(httpResponse.Body)
     if err := json.Unmarshal(body, &auction_response); err != nil {
-      bids <- auctionResponse{}
+      bids <- AuctionResponse{}
     } else {
       bids <- auction_response
     }
   } else {
-    bids <- auctionResponse{}
+    bids <- AuctionResponse{}
   }
 }
 
-func (as auctionService) ConductAuction(adPlacementId string) auctionResponse {
+func (as auctionService) ConductAuction(adPlacementId string) AuctionResponse {
   bidding_services := as.GetBiddingServices(adPlacementId)
 
-  bids := make(chan auctionResponse)
+  bids := make(chan AuctionResponse)
 
   for _, bs := range bidding_services {
     go as.MakeBidRequest(bs, adPlacementId, bids)
   }
 
-  max_bid_resp := auctionResponse{}
+  max_bid_resp := AuctionResponse{}
   count := 0
   for count != len(bidding_services) {
     bid := <-bids
@@ -116,13 +116,13 @@ func (as auctionService) ConductAuction(adPlacementId string) auctionResponse {
   if max_bid_resp.BidPrice >= 0 {
     return max_bid_resp
   } else {
-    return auctionResponse{}
+    return AuctionResponse{}
   }
 }
 
 func MakeAuctionEndpoint(as AuctionService) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(auctionRequest)
+		req := request.(AuctionRequest)
 		if as.ValidateAdPlacementId(req.AdPlacementId) {
       res := as.ConductAuction(req.AdPlacementId)
       if res.AdId == "" {
@@ -154,7 +154,7 @@ func decodeAuctionRequest(_ context.Context, r *http.Request) (interface{}, erro
   if r.Method != "POST" {
     return nil, errors.New("Only POST requests allowed.")
   }
-	var request auctionRequest
+	var request AuctionRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		return nil, err
 	}
